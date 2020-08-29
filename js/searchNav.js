69c0f7000a16539f314c3e04ae5584461c81bb56
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2020-08-29 09:40:44 -0500

    Added event listener to search nav button.

--- js/searchNav.js
+++ js/searchNav.js
@@ -1,9 +1,17 @@
 import { searchNav, box as searchBox } from './dom/searchBoxSelectors.js';
 import { getAndShowGIFs } from './searchGIFs.js';
 
+const searchBtn = document.querySelector('#search-nav-btn');
+
 searchNav.addEventListener('keyup', (evt) => {
   if (evt.which === 13) {
     searchBox.value = searchNav.value;
+    getAndShowGIFs(searchNav.value);
+  }
+});
+
+searchBtn.addEventListener('click', () => {
+  if (searchNav.value) {
     getAndShowGIFs(searchNav.value);
   }
 });
